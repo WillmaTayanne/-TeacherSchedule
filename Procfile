be3531ea90deae3b8e735127d58bb7e46b65810e
WillmaTayanne <willmatayanne@gmail.com>
2022-04-22 23:15:38 -0300

    atualizando WorkerProcess

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: java $JAVA_OPTS -jar target/dependency/webapp-runner.jar --port $PORT target/*.war
-worker: sh target/bin/worker-process
+worker: sh target/bin/worker
--- src/main/java/WorkerProcess.java
+++ src/main/java/WorkerProcess.java
@@ -6,22 +6,25 @@ public class WorkerProcess {
 
     private final static String QUEUE = "request-queue";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        try {
+            String uri = System.getenv("CLOUDAMQP_URL");
+            if (uri == null) uri = "amqps://fvbmyulz:sWunE0VdpHq0hXBh4N7qrZNKpQbtqgwL@beaver.rmq.cloudamqp.com/fvbmyulz";
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setUri(uri);
+            Channel channel = factory.newConnection().createChannel();
 
-        String uri = System.getenv("CLOUDAMQP_URL");
-        if (uri == null) uri = "amqps://fvbmyulz:sWunE0VdpHq0hXBh4N7qrZNKpQbtqgwL@beaver.rmq.cloudamqp.com/fvbmyulz";
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setUri(uri);
-        Channel channel = factory.newConnection().createChannel();
+            // Declare queue on which to listen for RPC calls
+            channel.queueDeclare(QUEUE, false, false, false, null);
 
-        // Declare queue on which to listen for RPC calls
-        channel.queueDeclare(QUEUE, false, false, false, null);
+            // Create RPC server
+            RpcServer server = new RpcServer(channel, QUEUE);
 
-        // Create RPC server
-        RpcServer server = new RpcServer(channel, QUEUE);
-
-        System.out.println(" [x] Awaiting requests");
-        server.mainloop();
+            System.out.println(" [x] Awaiting requests");
+            server.mainloop();
+        } catch(Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
 }
